5d3fefae5a4323f83b35c5eb6b6010ff5e34a953
Ravana ⭐ <47528708+ravana69@users.noreply.github.com>
2022-12-16 15:51:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rotator
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/clementGir/pen/mwRvyP](https://codepen.io/clementGir/pen/mwRvyP).
+
+TweenMax looping animation, inspirated by the trending @dave :) https://beesandbombs.tumblr.com/post/158720584694/rotator
+Do follow this guy.
---
+++ dist/script.js
@@ -0,0 +1,45 @@
+const $circOne = document.querySelector(".one");
+const $circTwo = document.querySelector(".two");
+
+const tl = new TimelineMax({
+  onComplete: () => {
+    tl.play(4);
+  } }).
+
+
+to($circTwo, 2, {
+  rotation: 360,
+  repeat: 1.25,
+  transformOrigin: "15px 140px",
+  ease: Linear.easeNone }).
+
+
+to($circTwo, 1, {
+  rotation: 90,
+  delay: -0.5,
+  width: "200px",
+  height: "200px",
+  ease: Power2.easeOut }).
+
+
+to($circTwo, 1, {
+  delay: -1,
+  x: 45,
+  y: -85 }).
+
+
+to($circOne, 1, {
+  delay: -1,
+  rotation: 0,
+  x: -125,
+  y: -10,
+  width: "30px",
+  height: "30px",
+  transformOrigin: "140px 15px",
+  ease: Power2.easeOut }).
+
+
+to($circOne, 4, {
+  delay: -1,
+  rotation: 810,
+  ease: Linear.easeNone });
